2e5cf6db82b5b67ad23b5210f5d75bcc029032b5
phoenixm103 <mooinuddinm103@gmail.com>
2020-06-24 16:52:58 +0600

    Updated test class

--- src/test/java/testProject/controller/TestApplicationControllerTest.java
+++ src/test/java/testProject/controller/TestApplicationControllerTest.java
@@ -7,36 +7,30 @@ import org.junit.jupiter.api.Test;
 
 
 public class TestApplicationControllerTest {
-	
+
 	TestApplicationController testApplicationController=new TestApplicationController();
-	
+
 		@Test
 		void testAddingTwoPositives() {
 			assertEquals(2, testApplicationController.add(1, 1), 
 					"Add method should return the sum of two numbers");
 		}
-		
+
 		@Test
 		void testMultiplyingTwoNumbers() {
 			assertEquals(10, testApplicationController.multiply(5, 2), 
 					"Multiply method should return the product of two numbers");
 		}
-		
+
 		@Test
 		void testAddingTwoNegatives() {
 			assertEquals(-2, testApplicationController.add(-1, -1), 
 					"Add method should return the sum of two numbers");
 		}
-		
+
 		@Test
 		void testAddingAPositiveAndANegative() {
 			assertEquals(0, testApplicationController.add(-1, 1), 
 					"Add method should return the sum of two numbers");
 		}
-//		@Test
-//		public void testHello() {
-//			String string="Junit";					
-//	        		
-//			assertEquals(string, testApplicationController.helloTest("Junit"));
-//		}
 }
